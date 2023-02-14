43757801fc59d0017e74cf8dbaac6d938bba61b9
Anudeepika23 <121333122+Anudeepika23@users.noreply.github.com>
2023-02-14 11:04:17 +0530

    Add files via upload

---
+++ NewProject/src/src/JavaPratice/For_Loop.java
@@ -0,0 +1,47 @@
+package JavaPratice;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+//		int a =10;
+//		int b = 20;
+//		for(int i=1;i<=10;i++) {
+//			System.out.println(i);
+//		}
+//	}
+//
+//}
+		
+//		//pattern using forloop
+//		for(int i=1;i<=4;i++) {
+//			for(int j=1;j<=3;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+		
+//			//Itreate  an arrray using for loop
+//		int[]arr= {2,3,4,5};
+//		for(int i =0;i<arr.length;i++) {
+//			System.out.println(arr[i]);
+//		}
+//		
+//	}
+//}
+		
+//		//Enhanced for loop
+//		int arr[]= {2,3,4,5};
+//		for(int num:arr) {
+//			System.out.println(num);
+//		}
+//	}
+//}
+		int sum = 0;  
+		for(int j = 1; j<=10; j++) {  
+		sum = sum + j;  
+		}  
+		System.out.println("The sum of first 10 natural numbers is " + sum);  
+		}  
+		}  
---
+++ NewProject/src/src/JavaPratice/WhileLoop.java
@@ -0,0 +1,32 @@
+package JavaPratice;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int i=0;
+//		System.out.println("Print the list of first even numbers 10");
+//		while(i<10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//	}
+//
+//}
+//		int i=0;
+//		System.out.println("Print the list of first even numbers 10");
+//		while(i<=10) {
+//			System.out.println(i);
+//			i=i+2;
+//		}
+//	}
+//
+//}
+		int arr[]= {2,3,4,5};
+		int i=0;
+		while(i<4) {
+			System.out.println(arr[i]);
+			i++;
+		}
+	}
+}
